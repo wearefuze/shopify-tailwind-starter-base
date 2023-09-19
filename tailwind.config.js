1b7d3b3a5e9568ce844f91a3b98c00efa67996f4
Matt Wilding <mattjwilding@gmail.com>
2023-09-19 12:29:01 +0100

    contact page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,16 @@ module.exports = {
       // sm: '320px',
       md: '750px',
       lg: '990px',
-      xlg: '1440px',
-      // x2lg: '1920px',
-      pageMaxWidth: '1440px',
+      xlg: '1280px',
+
+      pageMaxWidth: '1280px',
     },
     container: {
       center: true,
     },
     extend: {
       fontFamily: {
-        redhat: ['Red Hat Display'],
+        serif: ['Red Hat Display', 'serif'],
       },
       colors: {
         'core-white': '#ffffff',
